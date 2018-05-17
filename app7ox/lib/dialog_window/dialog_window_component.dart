3fbea5fc15cf909700f3f7a16e3ba50b172541cb
geooooo <geo97it@gmail.com>
2018-05-17 21:04:06 +0300

    append dialog-window

---
+++ app7ox/lib/dialog_window/dialog_window_component.dart
@@ -0,0 +1,9 @@
+import 'package:angular/angular.dart';
+
+
+@Component(
+  selector: 'dialog-window',
+  templateUrl: 'dialog_window_component.html',
+  styleUrls: const [ 'dialog_window_component.css' ],
+)
+class DialogWindowComponent {}
