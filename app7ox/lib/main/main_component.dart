b7cf6c634625d866cf92bd3111e6d07146ae2f21
geooooo <geo97it@gmail.com>
2018-05-17 17:59:34 +0300

    updates

--- app7ox/lib/main/main_component.dart
+++ app7ox/lib/main/main_component.dart
@@ -1,11 +1,21 @@
 import 'package:angular/angular.dart';
+import '../press_button/press_button_component.dart';
+import '../label/label_dark/label_dark_component.dart';
+import '../label/label_green/label_green_component.dart';
+import '../label/label_yellow/label_yellow_component.dart';
+import '../label/label_light/label_light_component.dart';
 
 
 @Component(
   selector: 'main',
   templateUrl: 'main_component.html',
   styleUrls: const [ 'main_component.css' ],
+  directives: const [
+    PressButtonComponent,
+    LabelDarkComponent,
+    LabelYellowComponent,
+    LabelGreenComponent,
+    LabelLightComponent,
+  ]
 )
-class MainComponent {
-
-}
+class MainComponent {}
