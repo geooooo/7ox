54d7d3e37ac17603d4a12aad6e2fd76b424b5535
geooooo <geo97it@gmail.com>
2018-05-17 17:59:03 +0300

    append button

---
+++ app7ox/lib/press_button/press_button_component.dart
@@ -0,0 +1,9 @@
+import 'package:angular/angular.dart';
+
+
+@Component(
+  selector: 'press-button',
+  templateUrl: 'press_button_component.html',
+  styleUrls: const [ 'press_button_component.css' ],
+)
+class PressButtonComponent {}
