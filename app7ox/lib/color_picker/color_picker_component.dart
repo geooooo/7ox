e24dd8cf53f202f8345b2728149a52d0bcab7e89
geooooo <geo97it@gmail.com>
2018-05-17 21:03:56 +0300

    append color-picker

---
+++ app7ox/lib/color_picker/color_picker_component.dart
@@ -0,0 +1,9 @@
+import 'package:angular/angular.dart';
+
+
+@Component(
+  selector: 'color-picker',
+  templateUrl: 'color_picker_component.html',
+  styleUrls: const [ 'color_picker_component.css' ],
+)
+class ColorPickerComponent {}
