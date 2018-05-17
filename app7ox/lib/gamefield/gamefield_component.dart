741304991b063149cdbd3a8b2130c62dd2fa926f
geooooo <geo97it@gmail.com>
2018-05-17 21:04:15 +0300

    append gamefield

---
+++ app7ox/lib/gamefield/gamefield_component.dart
@@ -0,0 +1,9 @@
+import 'package:angular/angular.dart';
+
+
+@Component(
+  selector: 'gamefield',
+  templateUrl: 'gamefield_component.html',
+  styleUrls: const [ 'gamefield_component.css' ],
+)
+class GamefieldComponent {}
