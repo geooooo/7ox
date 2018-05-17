9adfa2b0f0aae7c59a8cabf6a7f1cfeffa310f22
geooooo <geo97it@gmail.com>
2018-05-17 21:04:22 +0300

    append level-picker

---
+++ app7ox/lib/level_picker/level_picker_component.dart
@@ -0,0 +1,9 @@
+import 'package:angular/angular.dart';
+
+
+@Component(
+  selector: 'level-picker',
+  templateUrl: 'level_picker_component.html',
+  styleUrls: const [ 'level_picker_component.css' ],
+)
+class LevelPickerComponent {}
