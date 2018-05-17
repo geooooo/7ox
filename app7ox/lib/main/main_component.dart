3a84dc4224921be1124650020e534155f32c0de8
geooooo <geo97it@gmail.com>
2018-05-17 17:04:21 +0300

    init

---
+++ app7ox/lib/main/main_component.dart
@@ -0,0 +1,11 @@
+import 'package:angular/angular.dart';
+
+
+@Component(
+  selector: 'main',
+  templateUrl: 'main_component.html',
+  styleUrls: const [ 'main_component.css' ],
+)
+class MainComponent {
+
+}
---
+++ app7ox/web/app.dart
@@ -0,0 +1,9 @@
+import 'package:angular/angular.dart';
+import '../lib/main/main_component.dart';
+
+
+void main(List<String> args) {
+
+  bootstrap(MainComponent);
+
+}
