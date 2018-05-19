79bd8807bf2819223423e2d7d2de1787e89e4972
geooooo <geo97it@gmail.com>
2018-05-19 11:25:11 +0300

    updates

--- app7ox/web/app.dart
+++ app7ox/web/app.dart
@@ -1,9 +1,9 @@
 import 'package:angular/angular.dart';
-import '../lib/main/main_component.dart';
+import '../lib/app/app_component.dart';
 
 
 void main(List<String> args) {
 
-  bootstrap(MainComponent);
+  bootstrap(AppComponent);
 
 }
